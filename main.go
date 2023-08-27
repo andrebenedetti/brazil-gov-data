5a8814bfd219394287302f81908a016cad061746
dr31 <andredbenedetti@gmail.com>
2023-08-27 02:12:08 -0300

    wip

--- main.go
+++ main.go
@@ -1,58 +1,51 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"regexp"
-	"strings"
-)
+import "gov-data/feed"
 
 func main() {
-	url := "https://dadosabertos.rfb.gov.br/CNPJ/"
+	feed.GetAvailableFiles()
+	// url := "https://dadosabertos.rfb.gov.br/CNPJ/"
 
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
+	// resp, err := http.Get(url)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer resp.Body.Close()
 
-	b, err := io.ReadAll(resp.Body)
+	// b, err := io.ReadAll(resp.Body)
 
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 
-	r := regexp.MustCompile(`href="(?P<link>.*.zip)"`)
-	matches := r.FindAllStringSubmatch(string(b), -1)
+	// r := regexp.MustCompile(`href="(?P<link>.*.zip)"`)
+	// matches := r.FindAllStringSubmatch(string(b), -1)
 
-	if err := os.Mkdir("data", 0755); err != nil {
-		if !strings.Contains(err.Error(), "already exists") {
-			log.Fatalln(err)
-		}
-	}
+	// if err := os.Mkdir("data", 0755); err != nil {
+	// 	if !strings.Contains(err.Error(), "already exists") {
+	// 		log.Fatalln(err)
+	// 	}
+	// }
 
-	for _, match := range matches {
-		filename := strings.Split(match[0], "\"")[1]
+	// for _, match := range matches {
+	// 	filename := strings.Split(match[0], "\"")[1]
 
-		file, err := os.Create("./data/" + filename)
-		if err != nil {
-			log.Fatalln(err)
-		}
+	// 	file, err := os.Create("./data/" + filename)
+	// 	if err != nil {
+	// 		log.Fatalln(err)
+	// 	}
 
-		// Put content on file
-		resp, err := http.Get(url + filename)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
+	// 	// Put content on file
+	// 	resp, err := http.Get(url + filename)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	defer resp.Body.Close()
 
-		size, err := io.Copy(file, resp.Body)
-		fmt.Printf("Got file size %d", size)
+	// 	size, err := io.Copy(file, resp.Body)
+	// 	fmt.Printf("Got file size %d", size)
 
-		defer file.Close()
-	}
+	// 	defer file.Close()
+	// }
 
 }
