169c788c8d48790185e69007b0d841f66d76f640
drei <andredbenedetti@gmail.com>
2023-11-01 23:26:51 -0300

    log download progress

--- lib/pipeline/download.go
+++ lib/pipeline/download.go
@@ -1,6 +1,7 @@
 package pipeline
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -11,6 +12,9 @@ func Download(fileUrl string) []byte {
 	if err != nil {
 		log.Fatalf("Failed to get url %s\n", fileUrl)
 	}
+
+	length := resp.ContentLength
+	fmt.Println(length)
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
@@ -19,4 +23,39 @@ func Download(fileUrl string) []byte {
 	}
 
 	return body
+}
+
+func DownloadWithProgress(fileUrl string) {
+	resp, err := http.Get(fileUrl)
+	if err != nil {
+		log.Fatalf("Failed to get url %s\n", fileUrl)
+	}
+
+	length := resp.ContentLength
+	fmt.Printf("Downloading file with length: %d\n", length)
+	defer resp.Body.Close()
+
+	buf := make([]byte, 1024)
+	bytesRead := 0
+	lastLoggedValue := 0
+	logPercentStep := 1
+	fmt.Println("Progress: 0%")
+	for {
+		n, err := resp.Body.Read(buf)
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		bytesRead += n
+
+		if bytesRead*100/int(length)-logPercentStep > lastLoggedValue {
+			// not entirely accurate, but better UX
+			lastLoggedValue += logPercentStep
+			fmt.Printf("Progress: %d%% \n", lastLoggedValue)
+		}
+	}
 }
